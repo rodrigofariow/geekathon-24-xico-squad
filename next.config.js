f7005437968585e633271cc979554d37cd84413c
Rodrigo Faria <rodrigo.faria@cartrack.com>
2024-11-24 00:44:34 +0000

    bun

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
-    BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
-  },
-};
-
-module.exports = nextConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
+    BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
